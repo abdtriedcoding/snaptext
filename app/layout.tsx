8643980bf6eedee7e41c8ecd1f47b5ab469429b2
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-04-16 21:30:16 +0530

    integrate vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { constructMetadata } from "@/lib";
 import { Poppins } from "next/font/google";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const font = Poppins({ subsets: ["latin"], weight: ["500"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <main className="flex flex-1 max-w-2xl mx-auto w-full flex-col items-center justify-center text-center px-4 py-10">
           <Toaster richColors theme="system" />
           {children}
+          <Analytics />
         </main>
         <Footer />
       </body>
