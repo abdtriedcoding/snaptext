35e8045ebf1c0aa9133617d1eb4ff0a9484c3881
sidd-abdullah <siddabdullahofficial@gmail.com>
2024-04-16 11:47:04 +0530

    design home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        {children}
+        <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 h-full">
+          {children}
+        </main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <p className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out">
+        <b>96,434</b> text from image extraced so far
+      </p>
+      <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
+        Extract your text from image using AI
+      </h1>
+    </>
+  );
 }
